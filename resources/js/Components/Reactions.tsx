c88401524555aa8d0e44ed55c7df959ebe673afc
Vahan <vahan@Vahans-iMac.local>
2025-12-03 06:03:48 +0400

    corrected paths

--- resources/js/Components/Reactions.tsx
+++ resources/js/Components/Reactions.tsx
@@ -6,7 +6,7 @@ import {
     DropdownMenuContent,
     DropdownMenuItem,
     DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu';
+} from './ui/dropdown-menu';
 import ReactionsModal from './ReactionsModal';
 
 interface ReactionsProps {
